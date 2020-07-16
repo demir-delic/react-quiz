4006a9f289553e7277c3404dec2289701fd6d2a9
Guitarhub786 <codewithdrawal@gmail.com>
2020-07-16 19:03:08 +0100

    add Question and Answer components

---
+++ src/Answer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const answer = () => {
+  return (
+    <div>
+      <input type="radio" />
+      <p>42</p>
+    </div>
+  )
+
+}
+
+export default answer;
--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import Button from './Button';
+import Question from './Question';
+import Answer from './Answer';
 import './App.css';
 
 function App() {
   return (
-    <Button className="App">
-    </Button>
+
+    <div className="App">
+      <Question></Question>
+      <Answer></Answer>
+      <Answer></Answer>
+      <Answer></Answer>
+      <Button className="btn"></Button>
+    </div>
   );
 }
 
